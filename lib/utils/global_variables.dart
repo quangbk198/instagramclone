505233e58b72a2185ae78d3e7b7f52dc986604c3
ADMIN\Admin <huyquang031098@gmail.com>
2022-09-13 00:02:57 +0700

    create ui post card

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagramclone/screens/add_post_screen.dart';
+import 'package:instagramclone/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItem = [
-  Text('feed'),
+  FeedScreen(),
   Text('search'),
   AddPostScreen(),
   Text('noti'),
